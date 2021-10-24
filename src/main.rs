480912f68809cbf6b81353a31525fc80dd6bdf02
Cassandra McCarthy <cassie@7596ff.com>
2021-10-23 21:28:39 -0400

    chore: update to twilight 0.7

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,30 @@ use twilight_gateway::{Event, Intents, Shard};
 use twilight_http::Client;
 use twilight_model::id::{GuildId, RoleId};
 
-/// ID of the Twilight support and development guild.
-const TWILIGHT_GUILD_ID: GuildId = GuildId(745809834183753828);
-
-/// ID of the base role that everyone in the Twilight guild should have.
-const TWILIGHT_BASE_ROLE: RoleId = RoleId(745812265005219894);
-
 /// 5 different roles which we like to set to colors based on the season
 /// in North America.
-const COLOR_ROLES: [RoleId; 5] = [
-    RoleId(787463754823630871),
-    RoleId(787463294419992606),
-    RoleId(787463761102897153),
-    RoleId(786293768366063686),
-    RoleId(787463763540312097),
+const COLOR_ROLES: [u64; 5] = [
+    787463754823630871,
+    787463294419992606,
+    787463761102897153,
+    786293768366063686,
+    787463763540312097,
 ];
 
 /// Where the bot actually takes place.
 async fn run() -> Result<(), Box<dyn Error>> {
     // Initialize the tracing subscriber.
     tracing_subscriber::fmt::init();
+
+    // ID of the base role that everyone in the Twilight guild should have.
+    fn twilight_base_role() -> RoleId {
+        RoleId::new(745812265005219894).expect("non zero")
+    }
+
+    // ID of the Twilight support and development guild.
+    fn twilight_guild_id() -> GuildId {
+        GuildId::new(745809834183753828).expect("non zero")
+    }
 
     // Get the token from the environment.
     let token = env::var("DISCORD_TOKEN")?;
@@ -44,20 +48,20 @@ async fn run() -> Result<(), Box<dyn Error>> {
     while let Some(event) = events.next().await {
         match event {
             // Process new members.
-            Event::MemberAdd(member_add) if member_add.guild_id == TWILIGHT_GUILD_ID => {
+            Event::MemberAdd(member_add) if member_add.guild_id == twilight_guild_id() => {
                 let user_id = member_add.user.id;
 
                 // Add the base twilight role to all new members.
-                http.add_guild_member_role(TWILIGHT_GUILD_ID, user_id, TWILIGHT_BASE_ROLE)
+                http.add_guild_member_role(twilight_guild_id(), user_id, twilight_base_role())
                     .exec()
                     .await?;
 
                 // Choose a color role based off of the user's ID.
-                let choice = user_id.0 % 5;
+                let choice = COLOR_ROLES[(user_id.get() % 5) as usize];
                 http.add_guild_member_role(
-                    TWILIGHT_GUILD_ID,
+                    twilight_guild_id(),
                     user_id,
-                    COLOR_ROLES[choice as usize],
+                    RoleId::new(choice).expect("non zero"),
                 )
                 .exec()
                 .await?;
@@ -73,7 +77,7 @@ async fn run() -> Result<(), Box<dyn Error>> {
 ///
 /// It starts up the `run` function, catches and reports any error that
 /// might have happened, and exits.
-#[tokio::main]
+#[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn Error>> {
     if let Err(source) = run().await {
         eprintln!("{}", source);
