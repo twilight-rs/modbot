29cc4e8f10fc99c23e0e2756cdc067fc5df6cef3
Cassandra McCarthy <cassie@7596ff.com>
2022-01-26 01:54:09 -0500

    update to twilight 0.9

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,31 @@ use futures_util::stream::StreamExt;
 use std::{env, error::Error};
 use twilight_gateway::{Event, Intents, Shard};
 use twilight_http::Client;
-use twilight_model::id::{GuildId, RoleId};
+use twilight_model::id::{
+    marker::{GuildMarker, RoleMarker},
+    Id,
+};
 
 /// 5 different roles which we like to set to colors based on the season
 /// in North America.
-const COLOR_ROLES: [u64; 5] = [
-    787463754823630871,
-    787463294419992606,
-    787463761102897153,
-    786293768366063686,
-    787463763540312097,
+const COLOR_ROLES: [Id<RoleMarker>; 5] = [
+    Id::new(787463754823630871),
+    Id::new(787463294419992606),
+    Id::new(787463761102897153),
+    Id::new(786293768366063686),
+    Id::new(787463763540312097),
 ];
+
+/// ID of the base role that everyone in the Twilight guild should have.
+const TWILIGHT_BASE_ROLE: Id<RoleMarker> = Id::new(745812265005219894);
+
+/// ID of the support and development guild.
+const TWILIGHT_GUILD_ID: Id<GuildMarker> = Id::new(745809834183753828);
 
 /// Where the bot actually takes place.
 async fn run() -> Result<(), Box<dyn Error>> {
     // Initialize the tracing subscriber.
     tracing_subscriber::fmt::init();
-
-    // ID of the base role that everyone in the Twilight guild should have.
-    fn twilight_base_role() -> RoleId {
-        RoleId::new(745812265005219894).expect("non zero")
-    }
-
-    // ID of the Twilight support and development guild.
-    fn twilight_guild_id() -> GuildId {
-        GuildId::new(745809834183753828).expect("non zero")
-    }
 
     // Get the token from the environment.
     let token = env::var("DISCORD_TOKEN")?;
@@ -48,23 +47,21 @@ async fn run() -> Result<(), Box<dyn Error>> {
     while let Some(event) = events.next().await {
         match event {
             // Process new members.
-            Event::MemberAdd(member_add) if member_add.guild_id == twilight_guild_id() => {
+            Event::MemberAdd(member_add) if member_add.guild_id == TWILIGHT_GUILD_ID => {
                 let user_id = member_add.user.id;
 
                 // Add the base twilight role to all new members.
-                http.add_guild_member_role(twilight_guild_id(), user_id, twilight_base_role())
+                http.add_guild_member_role(TWILIGHT_GUILD_ID, user_id, TWILIGHT_BASE_ROLE)
                     .exec()
                     .await?;
 
                 // Choose a color role based off of the user's ID.
                 let choice = COLOR_ROLES[(user_id.get() % 5) as usize];
-                http.add_guild_member_role(
-                    twilight_guild_id(),
-                    user_id,
-                    RoleId::new(choice).expect("non zero"),
-                )
-                .exec()
-                .await?;
+
+                // Update the member's role.
+                http.add_guild_member_role(TWILIGHT_GUILD_ID, user_id, choice)
+                    .exec()
+                    .await?;
             }
             _ => {}
         }
